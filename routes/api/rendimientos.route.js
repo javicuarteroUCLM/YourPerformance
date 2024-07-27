bd9685c14791e4540c68fd9b769b111d796e08dc
Javi Cuartero <javier.cuartero@alu.uclm.es>
2024-07-27 22:11:26 +0200

    Creación de rutas PUT y DELETE y sus tests

--- routes/api/rendimientos.route.js
+++ routes/api/rendimientos.route.js
@@ -1,24 +1,46 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Rendimiento = require('../../models/performance.model');
+const Rendimiento = require("../../models/performance.model");
 
-router.get('/', async (req, res) => {
-    try {
-        const rendimientos = await Rendimiento.find();
-        res.json(rendimientos);
-    } catch (error) {
-        res.status(500).json({ error: 'Ha ocurrido un error' });
-        
-    }
+router.get("/", async (req, res) => {
+  try {
+    const rendimientos = await Rendimiento.find();
+    res.json(rendimientos);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
 });
 
-router.post('/', async (req, res) => {
-    try {
-        const newRendimiento = await Rendimiento.create(req.body);
-        res.json(newRendimiento);
+router.post("/", async (req, res) => {
+  try {
+    const newRendimiento = await Rendimiento.create(req.body);
+    res.json(newRendimiento);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
+});
 
+router.put("/:rendimientoId", async (req, res) => {
+  try {
+    const rendimientoEdit = await Rendimiento.findByIdAndUpdate(
+      req.params.rendimientoId,
+      req.body,
+      { new: true }
+    );
+    res.json(rendimientoEdit);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
+});
+
+router.delete("/:rendimientoId", async (req, res) => {
+    try {
+        const rendimientoDelete = await Rendimiento.findByIdAndDelete(req.params.rendimientoId);
+        res.json(rendimientoDelete);
+        
     } catch (error) {
-        res.status(500).json({ error: 'Ha ocurrido un error' });
+        res.status(500).json({ error: "Ha ocurrido un error" });
+        
     }
 });
 
--- test/api/rendimiento.route.spec.js
+++ test/api/rendimiento.route.spec.js
@@ -36,37 +36,102 @@ describe("Pruebas sobre la API de rendimiento", () => {
       years: [
         { year: 2020, asignaturas: [{ nota: 10, nota: 7, nota: 9, nota: 5 }] },
         { year: 2021, asignaturas: [{ nota: 9, nota: 6, nota: 4 }] },
-      ], name: 'test rendimiento'
+      ],
+      name: "test rendimiento",
     };
     const wrongRendimiento = {
-        years: [
-          { hola: 2020,},
-          { year: 2021, },
-        ], name: 'test rendimiento'
-      };
+      years: [{ hola: 2020 }, { year: 2021 }],
+      name: "test rendimiento",
+    };
     afterAll(async () => {
-      await Rendimiento.deleteMany({name: 'test rendimiento'});
+      await Rendimiento.deleteMany({ name: "test rendimiento" });
     });
 
     it("La ruta funciona correctamente", async () => {
-      const response = await request(app).post("/api/rendimientos").send(newRendimiento);
+      const response = await request(app)
+        .post("/api/rendimientos")
+        .send(newRendimiento);
 
       expect(response.status).toBe(200);
       expect(response.headers["content-type"]).toContain("json");
     });
 
     it("Se inserta correctamente datos para análisis de rendimiento", async () => {
-      const response = await request(app).post("/api/rendimientos").send(newRendimiento);
+      const response = await request(app)
+        .post("/api/rendimientos")
+        .send(newRendimiento);
 
       expect(response.body._id).toBeDefined();
       expect(response.body.years.length).toEqual(newRendimiento.years.length); // Se espera que el número de años sea igual
     });
 
     it("Pruebas para Error en la inserción de datos para análisis de rendimiento", async () => {
-      const response = await request(app).post("/api/rendimientos").send(wrongRendimiento);
+      const response = await request(app)
+        .post("/api/rendimientos")
+        .send(wrongRendimiento);
 
       expect(response.status).toBe(500);
       expect(response.body.error).toBeDefined();
     });
   });
+
+  describe("PUT /api/rendimientos/", () => {
+    let rendimiento;
+
+    beforeEach(async () => {
+      rendimiento = await Rendimiento.create({
+        years: [{ year: 2018, asignaturas: [{ nota: 5 }] }],
+        name: "test rendimiento",
+      });
+    });
+
+    afterEach(async() => {
+        await Rendimiento.findByIdAndDelete(rendimiento._id);
+    });
+
+    it("La ruta funciona correctamente", async () => {
+      const response = await request(app)
+        .put(`/api/rendimientos/${rendimiento._id}`)
+        .send({ name: 'rendimiento updated' });
+
+      expect(response.status).toBe(200);
+      expect(response.headers["content-type"]).toContain("json");
+    });
+
+    it ("Se actualiza correctamente el nombre del rendimiento", async () => {
+        const response = await request(app)
+        .put(`/api/rendimientos/${rendimiento._id}`)
+        .send({ name: 'rendimiento updated' });
+
+        expect(response.body._id).toBeDefined();
+        expect(response.body.name).toBe('rendimiento updated');
+        
+    });
+  });
+
+  describe("DELETE /api/rendimientos" , () => {
+
+    beforeEach(async() => {
+        rendimiento = await Rendimiento.create({
+            years: [{ year: 2023, asignaturas: [{ nota: 8, nota: 10 }] }],
+            name: "test rendimiento",
+          });
+
+        response = await request(app).delete(`/api/rendimientos/${rendimiento._id}`).send();
+    });
+
+    it("La ruta funciona correctamente", async () => {
+        expect(response.status).toBe(200);
+        expect(response.headers["content-type"]).toContain("json");
+    });
+  });
+
+  it ("Se elimina correctamente el rendimiento/informe", async () => {
+    expect(response.body._id).toBeDefined();
+
+    const foundRedimiento = await Rendimiento.findById(rendimiento._id);
+    expect(foundRedimiento).toBeNull();
+  });
+
+
 });
